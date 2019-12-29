1c8d5194b6abbc21737919e5c73f4826ef5deaf5
Francesco Balestrieri <francesco.balestrieri@intel.com>
2019-12-29 08:35:16 +0100

    Add texture to square and triangle

---
+++ textures/shader.cpp
@@ -0,0 +1,83 @@
+#include "shader.h"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+Shader::Shader(const GLchar* vsPath, const GLchar* fsPath)
+{
+	std::string vsCodeString;
+	std::string fsCodeString;
+	std::ifstream vsFile;
+	std::ifstream fsFile;
+
+	// ensure ifstream objects can throw exceptions:
+    vsFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+    fsFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+
+    // load shaders from files
+    try {
+    	vsFile.open(vsPath);
+    	std::stringstream vsStream;
+    	vsStream << vsFile.rdbuf();
+    	vsFile.close();
+    	vsCodeString = vsStream.str();
+
+    	fsFile.open(fsPath);
+    	std::stringstream fsStream;
+    	fsStream << fsFile.rdbuf();
+    	fsFile.close();
+    	fsCodeString = fsStream.str();
+    }
+    catch(std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+
+    const char* vsCode = vsCodeString.c_str();
+    const char* fsCode = fsCodeString.c_str();
+
+    unsigned int vertex, fragment;
+    int success;
+    char infoLog[512];
+
+    // compile shaders
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vsCode, NULL);
+	glCompileShader(vertex);
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+    	glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+    	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fsCode, NULL);
+	glCompileShader(fragment);
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+    	glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+    	std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// link shaders
+
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+	glGetProgramiv(ID, GL_LINK_STATUS, &success);
+	if(!success) {
+    	glGetProgramInfoLog(ID, 512, NULL, infoLog);
+    	    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+}
+
+void Shader::use()
+{
+	glUseProgram(ID);
+}
