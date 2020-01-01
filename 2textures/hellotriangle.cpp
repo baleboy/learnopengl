6946a2ef22eeeed0c76eb1031bcce192325b26f9
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-01-01 10:06:40 +0100

    Flip texture image on load

--- 2textures/hellotriangle.cpp
+++ 2textures/hellotriangle.cpp
@@ -98,6 +98,7 @@ void loadTextures(unsigned int& texture1, unsigned int& texture2)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+	stbi_set_flip_vertically_on_load(true);  
 	data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
