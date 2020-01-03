a01223da669b7c59e7bd6b05047e966e177ce289
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-01-03 09:18:48 +0100

    Remove unnecessary functions

--- coords/hellotriangle.cpp
+++ coords/hellotriangle.cpp
@@ -33,40 +33,6 @@ void processInput(GLFWwindow *window)
 
 	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
 		drawLines = !drawLines;	
-
-	if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS)
-		drawSquare = !drawSquare;	
-}
-
-unsigned int createTriangle()
-{
-	float vertices[] = {
-		// positions		// colors		  //texture coord
-    	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-     	0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-     	0.0f,  0.5f, 0.0f,	0.0f, 0.0f, 1.0f, 0.5f, 1.0f
-	};
-
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);  
-	glBindVertexArray(VAO);
-
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);  
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
-	glEnableVertexAttribArray(1);  	
-
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6* sizeof(float)));
-	glEnableVertexAttribArray(2);  	
-
-	return VAO;
 }
 
 void loadTextures(unsigned int& texture1, unsigned int& texture2)
@@ -115,47 +81,6 @@ void loadTextures(unsigned int& texture1, unsigned int& texture2)
 		std::cout << "Failed to load texture 2" << std::endl;
 	}
 
-}
-
-unsigned int createSquare()
-{
-	float vertices[] = {
-    	// positions          // colors           // texture coords
-    	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-    	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-	};
-
-	unsigned int indices[] = {  // note that we start from 0!
-    	0, 1, 3,   // first triangle
-    	1, 2, 3    // second triangle
-	};
-
-	unsigned int VAO, VBO, EBO;
-
-	glGenVertexArrays(1, &VAO);  
-	glBindVertexArray(VAO);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);  
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
-	glEnableVertexAttribArray(1);  
-	// texture attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);    
-
-	return VAO;
 }
 
 unsigned int createCube()
