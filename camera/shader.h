1b9e337ee86108293dbca39b769ff115e6fa67b9
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-01-04 10:25:39 +0100

    Add camera tutorial. Rotate camera around origin

---
+++ camera/shader.h
@@ -0,0 +1,25 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader {
+public:
+
+	unsigned int ID;
+
+	Shader(const GLchar* vertxShaderPath, const GLchar* fragmentShaderPath);
+
+	void use();
+
+	void setInt(const std::string &name, int value) const;
+
+	void setMat4(const std::string &name, const glm::mat4 &matrix) const;
+
+};
+
+#endif
