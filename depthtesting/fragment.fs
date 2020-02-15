9a4679d2402515662d8e613cf993b3db734410d0
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-02-15 08:11:08 +0200

    Show depth value as shades of gray in fragment shader

--- depthtesting/fragment.fs
+++ depthtesting/fragment.fs
@@ -6,7 +6,17 @@ in vec2 ourTexCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
+float near = 0.1; 
+float far  = 100.0; 
+  
+float LinearizeDepth(float depth) 
+{
+    float z = depth * 2.0 - 1.0; // back to NDC 
+    return (2.0 * near * far) / (far + near - z * (far - near));	
+}
+
 void main()
 {
-	FragColor = mix(texture(texture1, ourTexCoord), texture(texture2, ourTexCoord), 0.2);
+	FragColor = vec4(vec3(LinearizeDepth(gl_FragCoord.z) / far), 1.0);
+	// FragColor = mix(texture(texture1, ourTexCoord), texture(texture2, ourTexCoord), 0.2);
 }
