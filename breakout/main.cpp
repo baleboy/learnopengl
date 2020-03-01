2c04474350317a1c975cd0d18d7cd90eeeb28b53
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-03-01 09:09:02 +0200

    Create window and call game class entry points in main loop

--- breakout/main.cpp
+++ breakout/main.cpp
@@ -9,7 +9,14 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <stb_image.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+#include "game.h"
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint SCREEN_WIDTH = 800;
+const GLuint SCREEN_HEIGHT = 600;
+
+Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 int main()
 {
@@ -25,8 +32,9 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 #endif
   	glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -34,6 +42,7 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
 	{
@@ -41,22 +50,43 @@ int main()
 		return -1;
 	}
 
-	glViewport(0,0,800,600);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glViewport(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+	glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	while(!glfwWindowShouldClose(window))
-	{ 
+    Breakout.Init();
+    GLfloat deltaTime = 0.0f;
+    GLfloat lastFrame = 0.0f;
 
-		glfwSwapBuffers(window);		
-		glfwPollEvents();
+    // Start Game within Menu State
+    Breakout.State = Game::GAME_MENU;
 
-	}
+    while (!glfwWindowShouldClose(window))
+    {
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        glfwPollEvents();
+
+        Breakout.ProcessInput(deltaTime);
+
+        Breakout.Update(deltaTime);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        Breakout.Render();
+
+        glfwSwapBuffers(window);
+    }
 
 	glfwTerminate();
 	return 0;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-	glViewport(0, 0, width, height);
+    // When a user presses the escape key, we set the WindowShouldClose property to true, closing the application
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
