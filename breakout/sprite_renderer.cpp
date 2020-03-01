ffcacd243992c78ac85be5c83f1971088e311aa4
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-03-01 11:07:40 +0200

    Add render function to sprite renderer

--- breakout/sprite_renderer.cpp
+++ breakout/sprite_renderer.cpp
@@ -27,3 +27,28 @@ void SpriteRenderer::initRenderData()
     glBindBuffer(GL_ARRAY_BUFFER, 0);  
     glBindVertexArray(0);
 }
+
+void SpriteRenderer::DrawSprite(Texture2D &texture, glm::vec2 position, 
+  glm::vec2 size, GLfloat rotate, glm::vec3 color)
+{
+    // Prepare transformations
+    this->shader.Use();
+    glm::mat4 model;
+    model = glm::translate(model, glm::vec3(position, 0.0f));  
+
+    model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f)); 
+    model = glm::rotate(model, rotate, glm::vec3(0.0f, 0.0f, 1.0f)); 
+    model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));
+
+    model = glm::scale(model, glm::vec3(size, 1.0f)); 
+  
+    this->shader.SetMatrix4("model", model);
+    this->shader.SetVector3f("spriteColor", color);
+  
+    glActiveTexture(GL_TEXTURE0);
+    texture.Bind();
+
+    glBindVertexArray(this->quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+} 
