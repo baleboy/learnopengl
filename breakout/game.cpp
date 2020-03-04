0c768b3cf125c8fd8037ee6839956a6b18cc8e1b
Francesco Balestrieri <francesco.balestrieri@intel.com>
2020-03-04 10:10:21 +0200

    Move paddle left right with keyboard

--- breakout/game.cpp
+++ breakout/game.cpp
@@ -1,3 +1,6 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include "game.h"
 #include "resource_manager.h"
 #include "shader.h"
@@ -54,7 +57,21 @@ void Game::Init()
 
 void Game::ProcessInput(GLfloat dt)
 {
-
+	if (this->State == GAME_ACTIVE)
+    {
+        GLfloat velocity = PLAYER_VELOCITY * dt;
+        // Move playerboard
+        if (this->Keys[GLFW_KEY_A])
+        {
+            if (Player->Position.x >= 0)
+                Player->Position.x -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_D])
+        {
+            if (Player->Position.x <= this->Width - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+    }
 }
 
 void Game::Update(GLfloat dt)
